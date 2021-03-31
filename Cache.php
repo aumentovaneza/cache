52f27e8f963907b39b2589866dc7c8274305b3f1
Vaneza Aumento <vaneza.aumento@gmail.com>
2021-04-01 01:17:48 +0800

    comments

--- Cache.php
+++ Cache.php
@@ -5,19 +5,21 @@ class Cache
 {
     public function saveCache()
     {
+        // Declare
         $timeout = 600000;
-
         $url = $_SERVER['API_ENDPOINT'];
         $name = explode('/', $url);
         $file = $name[count($name) -1];
         $cache = 'cachefile-'.substr_replace($file,"",-4).'.html';
 
+        //check if there is a set cache time, if not use default
         if(!empty($_REQUEST['CACHE_TIME'])){
             $timeout = $_REQUEST['CACHE_TIME'];
         }
 
+        //Execute
         if(file_exists($cache) && time() - $timeout < filemtime($cache)){
-            echo "<!-- Cache generated ".date('H:i', filemtime($cache))." -->\n";
+            echo "<!-- Cache generated for endpoint".date('H:i', filemtime($cache))." -->\n";
             readfile($cache);
             exit;
         }
